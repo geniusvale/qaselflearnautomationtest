fe31be1602bfb2171358c29ebdca196629d2a19c
geniusvale <ariiqromero@gmail.com>
2025-04-28 22:12:21 +0700

    remove import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
-from selenium.webdriver.edge.service import Service
 
 import time
 
